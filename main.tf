9225b32cdc0f91741a2c63a6982c46cef28f1908
gary.ma <garymar2007@gmail.com>
2024-06-18 22:38:29 +0200

    create vpc and subnet.

--- main.tf
+++ main.tf
@@ -3,10 +3,27 @@ provider "aws" {
 }
 
 # Create a new EC2 instance
-resource "aws_instance" "my-first-server" {
-  ami           = "ami-04b70fa74e45c3917"
-  instance_type = "t2.micro"
-  tags = {
-    Name = "my-first-server"
+#resource "aws_instance" "my-first-server" {
+#  ami           = "ami-04b70fa74e45c3917"
+#  instance_type = "t2.micro"
+#  tags = {
+#    Name = "my-first-server"
+#  }
+#}
+
+# Create a VPC
+resource "aws_vpc" "my-first-vpc" {
+  cidr_block = "10.0.0.0/16"
+  tags       = {
+    Name = "production-vpc"
+  }
+}
+
+# Create a subnet
+resource "aws_subnet" "subnet1" {
+  vpc_id     = aws_vpc.my-first-vpc.id
+  cidr_block = "10.0.0.0/24"
+  tags       = {
+    Name = "production-subnet"
   }
 }
